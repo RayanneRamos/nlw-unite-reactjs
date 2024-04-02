306e18e6ee04895a6d5fdd8e17ea63b574b94fc6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-04-02 15:47:41 -0300

    criando o header da aplicação

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import logoImageSVG from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={logoImageSVG} alt="logo" />
+      <nav className="flex items-center gap-5">
+        <a href="#" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="#" className="font-medium text-sm">
+          Participantes
+        </a>
+      </nav>
+    </div>
+  );
+}
