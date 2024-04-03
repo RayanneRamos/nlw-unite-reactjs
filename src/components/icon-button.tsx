88a036b6cadc751f55620d7c3b38c178d5bc6ea2
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-04-03 10:56:30 -0300

    usando o twMerge para adicionar opacity nos botões

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,18 +1,28 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<"button"> {
   transparent?: boolean;
 }
 
-export function IconButton({ transparent, ...props }: IconButtonProps) {
+export function IconButton({
+  transparent,
+  disabled,
+  ...props
+}: IconButtonProps) {
   return (
     <button
-      className={
-        transparent
-          ? "bg-black/20 border border-white/10 rounded-md p-1.5"
-          : "bg-white/10 border border-white/10 rounded-md p-1.5"
-      }
       {...props}
+      //className={
+      //  transparent
+      //    ? "bg-black/20 border border-white/10 rounded-md p-1.5"
+      //    : "bg-white/10 border border-white/10 rounded-md p-1.5"
+      //}
+      className={twMerge(
+        "border border-white/10 rounded-md p-1.5",
+        transparent ? "bg-black/10" : "bg-white/10",
+        disabled ? "opacity-50" : null
+      )}
     />
   );
 }
